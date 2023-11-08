7c73c604c1239e0b06760473d884951b05867ded
Skarader <lukasholmwolf@gmail.com>
2023-11-08 23:47:13 +0100

    gjort function till att addera nya posts

--- script.js
+++ script.js
@@ -8,11 +8,14 @@ fetch("https://dummyjson.com/posts/")
     renderPosts(data.posts);
   });
 
+let postArray = []; //
+
 function renderPosts(posts) {
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < posts.length; i++) {
     let post = posts[i];
 
     let createNewPost = document.createElement("div");
+    postArray.push(createNewPost); //
     createNewPost.classList.add("post-container");
 
     let postTitle = document.createElement("h3");
@@ -40,7 +43,8 @@ function renderPosts(posts) {
 
     postTagsDiv.append(postTags, postTags1, postTags2);
     createNewPost.append(postTitle, postBody, postTagsDiv);
-    parent.append(createNewPost);
+
+    parent.append(postArray[i]);
   }
 }
 
@@ -55,6 +59,57 @@ createButton.addEventListener("click", function () {
 });
 
 closeButton.addEventListener("click", function () {
+  newPostWindow.classList.remove("flex");
+  overlay.classList.remove("flex");
+});
+
+let sendPostButton = document.querySelector(".send-post");
+
+sendPostButton.addEventListener("click", function () {
+  let title = document.querySelector(".newPostTitle");
+  let text = document.querySelector(".newPostText");
+  let tags1 = document.querySelector(".newPostTags1");
+  let tags2 = document.querySelector(".newPostTags2");
+  let tags3 = document.querySelector(".newPostTags3");
+
+  let createNewPost = document.createElement("div");
+  postArray.unshift(createNewPost);
+  createNewPost.classList.add("post-container");
+
+  let postTitle = document.createElement("h3");
+  postTitle.classList.add("post-title");
+  postTitle.innerText = title.value;
+
+  let postBody = document.createElement("span");
+  postBody.classList.add("post-body");
+  postBody.innerText = text.value;
+
+  let postTagsDiv = document.createElement("div");
+  postTagsDiv.classList.add("post-tags-container");
+
+  let postTags = document.createElement("span");
+  let postTags1 = document.createElement("span");
+  let postTags2 = document.createElement("span");
+  postTags.classList.add("post-tags");
+  postTags1.classList.add("post-tags");
+  postTags2.classList.add("post-tags");
+
+  postTags.innerText = tags1.value + " ";
+  postTags1.innerText = tags2.value + " ";
+  postTags2.innerText = tags3.value;
+
+  postTagsDiv.append(postTags, postTags1, postTags2);
+  createNewPost.append(postTitle, postBody, postTagsDiv);
+
+  parent.insertBefore(createNewPost, postArray[1]);
+
+  document.querySelector(".newPostTitle").value = "";
+  document.querySelector(".newPostText").value = "";
+  document.querySelector(".newPostTags1").value = "";
+  document.querySelector(".newPostTags2").value = "";
+  document.querySelector(".newPostTags3").value = "";
+
+  // Close the new post window
   newPostWindow.classList.remove("flex");
   overlay.classList.remove("flex");
 });
